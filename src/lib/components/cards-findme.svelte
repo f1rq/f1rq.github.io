c1cccb8baa14ab3e7d72a517613f83f9b3594cd4
Fabio <forgistm.pl@gmail.com>
2023-11-24 21:11:31 +0100

    .

--- src/lib/components/cards-findme.svelte
+++ src/lib/components/cards-findme.svelte
@@ -1,50 +1,50 @@
-<div class="grid gap-4 grid-cols-2">
+<div class="grid gap-4 grid-cols-3">
     <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
-          <h2 class="card-title text-3xl">E-Mail</h2>
-          <p>If you want to contact me via E-Mail, send it to <span class="text-accent">f1rq@duck.com</span></p>
+          <h2 class="card-title text-3xl">Github</h2>
+          <p>My github: <span class="text-accent">github.com/f1rq</span></p>
           <div class="card-actions justify-end">
-            <a class="btn btn-primary" href="https://google.com" >click to copy</a>
+            <a class="btn btn-primary" href="https://github.com/f1rq" >github.com</a>
           </div>
         </div>
     </div>
 
     <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
-          <h2 class="card-title text-3xl">E-Mail</h2>
-          <p>If you want to contact me via E-Mail, send it to <span class="text-accent">f1rq@duck.com</span></p>
+          <h2 class="card-title text-3xl">X</h2>
+          <p>My X: <span class="text-accent">x.com/_f1rq_</span></p>
           <div class="card-actions justify-end">
-            <a class="btn btn-primary" href="https://google.com" >click to copy</a>
+            <a class="btn btn-primary" href="https://x.com/_f1rq_">x.com</a>
           </div>
         </div>
     </div>
 
     <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
-          <h2 class="card-title text-3xl">E-Mail</h2>
-          <p>If you want to contact me via E-Mail, send it to <span class="text-accent">f1rq@duck.com</span></p>
+          <h2 class="card-title text-3xl">Instagram</h2>
+          <p>My Instagram: <span class="text-accent">instagram.com/_f1rq_</span></p>
           <div class="card-actions justify-end">
-            <a class="btn btn-primary" href="https://google.com" >click to copy</a>
+            <a class="btn btn-primary" href="https://instagram.com/_f1rq_">instagram.com</a>
           </div>
         </div>
     </div>
 
     <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
-          <h2 class="card-title text-3xl">E-Mail</h2>
-          <p>If you want to contact me via E-Mail, send it to <span class="text-accent">f1rq@duck.com</span></p>
+          <h2 class="card-title text-3xl">YouTube</h2>
+          <p>My YouTube: <span class="text-accent">youtube.com/@_f1rq_</span></p>
           <div class="card-actions justify-end">
-            <a class="btn btn-primary" href="https://google.com" >click to copy</a>
+            <a class="btn btn-primary" href="https://youtube.com/@_f1rq_" >youtube.com</a>
           </div>
         </div>
     </div>
 
     <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
-          <h2 class="card-title text-3xl">E-Mail</h2>
-          <p>If you want to contact me via E-Mail, send it to <span class="text-accent">f1rq@duck.com</span></p>
+          <h2 class="card-title text-3xl">Google Dev Profile</h2>
+          <p>My G Dev profile: <span class="text-accent">g.dev/f1rq</span></p>
           <div class="card-actions justify-end">
-            <a class="btn btn-primary" href="https://google.com" >click to copy</a>
+            <a class="btn btn-primary" href="https://g.dev/f1rq" >google.dev</a>
           </div>
         </div>
     </div>
