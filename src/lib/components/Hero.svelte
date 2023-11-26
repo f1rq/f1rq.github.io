72236bf02deb9a4b0b0ecbde5e28102706361d7d
Fabio <forgistm.pl@gmail.com>
2023-11-26 11:33:25 +0100

    Update Hero.svelte

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,6 +1,13 @@
 <script>
   import Saos from "saos";
 </script>
+
+<style>
+  .shadow-effect:hover {
+    box-shadow: -6px 6px 0 0 #1f2c47;
+  }
+</style>
+
 <div class="hero min-h-screen">
   <Saos animation={"swing-in-top-fwd 0.8s cubic-bezier(0.35, 0.5, 0.65, 0.95) both"}>
   <div class="hero-content text-center">
@@ -8,8 +15,8 @@
       <h1 class="text-5xl font-bold">Hi, I'm <span class="text-accent">f1rq</span></h1>
       <p class="py-6">I'm a beginner programmer and passionate gamer from Poland. I'm a beginner programmer who likes especially webdev, from the front-end site.</p>
       <div class="space-x-5">
-      <a class="btn btn-primary" href="#contact">Contact with me</a>
-      <a class="btn btn-primary" href="#findme">Find me anywhere</a>
+      <a class="btn btn-primary shadow-effect" href="#contact">Contact with me</a>
+      <a class="btn btn-primary shadow-effect" href="#findme">Find me anywhere</a>
     </div>
     </div>
   </div>
