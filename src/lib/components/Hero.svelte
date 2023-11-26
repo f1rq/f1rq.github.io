cff9e4f111f6e0b5041af0dbefcbfff3095a7ffd
Fabio <forgistm.pl@gmail.com>
2023-11-26 14:29:57 +0100

    Update Hero.svelte

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -9,16 +9,18 @@
 </style>
 
 <div class="hero min-h-screen">
-  <Saos animation={"swing-in-top-fwd 0.8s cubic-bezier(0.35, 0.5, 0.65, 0.95) both"}>
   <div class="hero-content text-center">
     <div class="max-w-md">
+      <Saos animation={"swing-in-top-fwd 0.8s cubic-bezier(0.35, 0.5, 0.65, 0.95) both"}>
       <h1 class="text-5xl font-bold">Hi, I'm <span class="text-accent">f1rq</span></h1>
       <p class="py-6">I'm a beginner programmer and passionate gamer from Poland. I'm a beginner programmer who likes especially webdev, from the front-end site.</p>
+      </Saos>
+      <Saos animation={"swing-in-top-fwd 0.8s cubic-bezier(0.35, 0.5, 0.65, 0.95) both"}>
       <div class="space-x-5">
       <a class="btn btn-primary shadow-effect" href="#contact">Contact with me</a>
       <a class="btn btn-primary shadow-effect" href="#findme">Find me anywhere</a>
     </div>
+    </Saos>
     </div>
   </div>
-</Saos>
 </div>
