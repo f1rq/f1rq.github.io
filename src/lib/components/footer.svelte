5a71531b70e5f0ec6af4dad19739672493b772ca
Fabio <forgistm.pl@gmail.com>
2023-11-29 20:32:13 +0100

    .

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,19 @@
+<script>
+  import Sveltelogo from "$lib/img/svelte-logo.svg"
+</script>
+
+<style>
+  .badge-svelte {
+    background: rgb(136, 34, 0)
+    }
+</style>
+
 <footer class="footer footer-center p-4 bg-base-200 text-base-content">
     <aside>
       <p>website by <span class="font-bold">f1rq</span> - 2023</p>
+      <div class="badge badge-svelte gap-2">
+        <img src="{Sveltelogo}" alt="sveltelogo" width="18px" height="18px">
+        Made with Svelte
+      </div>
     </aside>
   </footer>
