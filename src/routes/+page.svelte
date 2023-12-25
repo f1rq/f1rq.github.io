c69e2ac74822943f931fcd0c37684b539f1974a6
Fabio <forgistm.pl@gmail.com>
2023-12-26 00:21:28 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
     import Hero from "$lib/components/Hero.svelte";
     import CardsContact from "$lib/components/cards-contact.svelte";
-    import CardsFindme from "$lib/components/cards-findme.svelte";
     import NavBar from "$lib/components/NavBar.svelte";
     import Footer from "$lib/components/footer.svelte";
     import Hobbys from "$lib/components/hobbys.svelte";
