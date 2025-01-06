b1a6871c8a98ddbffd4c52d0c9e6b106af520b25
Fabio <forgistm.pl@gmail.com>
2025-01-06 20:11:42 +0100

    .

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,6 @@ import adapter from '@sveltejs/adapter-vercel';
 const config = {
 	kit: {
 		adapter: adapter(),
-		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/f1rq.github.io' : '',
-        }
 	}
 };
 
