1debdca5e125963c6daa0b6827f6078d68f1112e
Fabio <forgistm.pl@gmail.com>
2025-01-06 15:47:30 +0100

    .

---
+++ dist/components/navigations.svelte
@@ -0,0 +1,36 @@
+<script>
+    import '@fortawesome/fontawesome-free/css/all.min.css'
+</script>
+
+<style>
+    a {
+        transition: background-color 0.2s;
+        border-radius: 20%;
+        i {
+            transition: transform 0.2s;
+
+        }
+        
+    }
+    a:hover {
+        i {
+            transform: scale(1.15);
+        }
+        background-color: rgba(255, 255, 255, 0.1);
+    }
+</style>
+
+<div class="btm-nav max-w-[16rem] max-h-[4rem] place-self-center mb-3 rounded-lg text-xl shadow-2xl bg-primary ">
+    <a aria-label="Home" href="/">
+        <i class="fa-solid fa-house"></i>
+    </a>
+    <a aria-label="Contact" href="/contact">
+        <i class="fa-solid fa-address-book"></i>
+    </a>
+    <a aria-label="Skills" href="/skills">
+        <i class="fa-solid fa-tools"></i>
+    </a>
+    <a aria-label="Projects" href="/projects">
+        <i class="fa-solid fa-suitcase"></i>
+    </a>
+</div>
---
+++ dist/components/sbau_body_card.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { cn } from "../utils";
+  
+    let className = "";
+    export { className as class };
+    let card = {
+      title: "Website for my uncle's business",
+      desc: "A website for my uncle's business. It's purpose is to be like a business card, but on the internet.",
+  
+    };
+  </script>
+  
+  <div class={cn("px-2 text-gray-100 sm:px-4 py-0 sm:pb-3 text-left", className)}>
+    <h3 class="text-lg font-semibold tracking-tighter mt-3 mb-1">
+      {card.title}
+    </h3>
+    <p class="text-sm leading-5">{card.desc}</p>
+  </div>
